7b70b761440e35a990619420f20da350544cdc67
Anurag Goel <anurag@render.com>
2019-06-06 09:49:36 -0700

    Display nodes on homepage

--- lib/elixir_cluster_demo_web/controllers/page_controller.ex
+++ lib/elixir_cluster_demo_web/controllers/page_controller.ex
@@ -2,6 +2,8 @@ defmodule ElixirClusterDemoWeb.PageController do
   use ElixirClusterDemoWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    self_node = inspect(node())
+    nodes = inspect(Node.list())
+    render(conn, "index.html", %{node: self_node, nodes: nodes})
   end
 end
